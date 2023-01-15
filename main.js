efbd12be8a3e956aabb4ad56812ae63572932d6e
hiroiruka <121372463+hiroiruka@users.noreply.github.com>
2023-01-15 23:06:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ const smoothScroll = () => {
                         },
                         speed: 500,
                         effect: 'fade',
-                    });
+                    }))
                 
                     smoothScroll();
                 }
